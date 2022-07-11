12e4ec76ec4a1dd0b1b5efd65a0716e8d4a1bffa
Maksym150678 <civicm110@gmail.com>
2022-07-11 16:29:07 +0300

    task 8 1 1630 110722

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryList = document.querySelector('.gallery');
+const galleryCardsArr = galleryItems
+.map(
+    (el) => 
+    `<div class="gallery__link">
+    <a class="gallery__link" href="${el.original}">
+      <img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source="${el.original}"
+        alt="${el.description}"
+      />
+    </a>
+  </div>`
+)
+.join("");
 
-console.log(galleryItems);
+
+galleryList.innerHTML = galleryCardsArr;
+
+
+ 
+
+galleryList.insertAdjacentHTML("afterbegin", galleryCardsArr);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+captionsData: "alt",
+captionPosition: "bottom",
+captionDelay: 250,
+});
